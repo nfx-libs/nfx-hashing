30cdb5b8f2dc24ac29121494324ef2c863c30c91
Ronan F. <ronan.fesselier@etik.com>
2025-11-13 21:04:16 +0100

    chore(cmake): remove unused PUBLIC_HEADERS variable

--- cmake/nfxHashingSources.cmake
+++ cmake/nfxHashingSources.cmake
@@ -3,26 +3,6 @@
 #==============================================================================
 
 #----------------------------------------------
-# Conditional headers and sources
-#----------------------------------------------
-
-set(PUBLIC_HEADERS)
-
-list(APPEND PUBLIC_HEADERS
-	${NFX_HASHING_INCLUDE_DIR}/nfx/hashing/Algorithms.h
-	${NFX_HASHING_INCLUDE_DIR}/nfx/hashing/Concepts.h
-	${NFX_HASHING_INCLUDE_DIR}/nfx/hashing/Constants.h
-	${NFX_HASHING_INCLUDE_DIR}/nfx/hashing/Hash.h
-	${NFX_HASHING_INCLUDE_DIR}/nfx/hashing/Hasher.h
-
-	${NFX_HASHING_INCLUDE_DIR}/nfx/detail/hashing/Algorithms.inl
-	${NFX_HASHING_INCLUDE_DIR}/nfx/detail/hashing/Hash.inl
-	${NFX_HASHING_INCLUDE_DIR}/nfx/detail/hashing/Hasher.inl
-
-	${NFX_HASHING_INCLUDE_DIR}/nfx/Hashing.h
-)
-
-#----------------------------------------------
 # Library definition
 #----------------------------------------------
 
