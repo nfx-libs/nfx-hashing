13de30b6b4a1ff08044ea69780807c7b6b3b6838
Ronan F․ <ronan.fesselier@etik.com>
2026-02-14 13:25:38 +0100

    chore(release): bump version to 0.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -31,6 +31,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - NIL
 
+## [0.3.1] - 2026-02-14
+
+### Fixed
+
+- **Critical**: Removed strict compiler warnings from library INTERFACE target
+
 ## [0.3.0] - 2026-02-14
 
 ### Added
--- cmake/nfxHashingTarget.cmake
+++ cmake/nfxHashingTarget.cmake
@@ -29,17 +29,6 @@ target_compile_features(${PROJECT_NAME}
 )
 
 #----------------------------------------------
-# Compiler warnings
-#----------------------------------------------
-
-# --- Enable strict warnings ---
-target_compile_options(${PROJECT_NAME}
-    INTERFACE
-        $<$<CXX_COMPILER_ID:MSVC>:/W4 /WX>
-        $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wall -Wextra -Werror>
-)
-
-#----------------------------------------------
 # SIMD optimization flags for hardware-accelerated hashing
 #----------------------------------------------
 
@@ -52,7 +41,7 @@ if(NFX_HASHING_ENABLE_SIMD)
         set(CMAKE_REQUIRED_FLAGS "-march=native")
         check_cxx_source_compiles("int main() { return 0; }" COMPILER_SUPPORTS_MARCH_NATIVE)
         unset(CMAKE_REQUIRED_FLAGS)
-        
+
         if(COMPILER_SUPPORTS_MARCH_NATIVE)
             target_compile_options(${PROJECT_NAME}
                 INTERFACE
